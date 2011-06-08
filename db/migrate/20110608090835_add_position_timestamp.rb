e74a2117bacee2eccfcad0ae867cd84907ceaf25
Beau Collins <beaucollins@gmail.com>
2011-06-08 09:17:39 -0700

    postgres incompatible fix

--- db/migrate/20110608090835_add_position_timestamp.rb
+++ db/migrate/20110608090835_add_position_timestamp.rb
@@ -2,7 +2,7 @@ class AddPositionTimestamp < ActiveRecord::Migration
   def self.up
     add_column :positions, :timestamp, :datetime
     
-    Position.update_all("`timestamp` = `created_at`")
+    Position.update_all("timestamp = created_at")
   end
 
   def self.down
