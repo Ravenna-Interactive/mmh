519210bfa70d96f3cfbc028444c38d709520ea84
Beau Collins <beaucollins@gmail.com>
2011-06-26 10:49:25 -0700

    trying to get the counter_cache working

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110626174313) do
+ActiveRecord::Schema.define(:version => 20110626165824) do
 
   create_table "hunts", :force => true do |t|
     t.integer  "map_id"
@@ -19,8 +19,8 @@ ActiveRecord::Schema.define(:version => 20110626174313) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.datetime "started_at"
-    t.datetime "last_recorded_at"
     t.integer  "positions_count",  :default => 0
+    t.datetime "last_recorded_at"
   end
 
   create_table "maps", :force => true do |t|
